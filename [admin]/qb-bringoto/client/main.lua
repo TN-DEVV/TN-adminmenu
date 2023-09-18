e5dcd87351e3a46ff80d8230e1b5477e2f5f8da5
DON <93664733+TN-DEVV@users.noreply.github.com>
2023-09-18 15:35:12 +0100

    Delete [admin] directory

--- [admin]/qb-bringoto/client/main.lua
+++
@@ -1,7 +0,0 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-
-RegisterNetEvent('qb-admin:client:bringTp')
-AddEventHandler('qb-admin:client:bringTp', function(coords)
-    local ped = GetPlayerPed(-1)
-    SetEntityCoords(ped, coords.x, coords.y, coords.z)
-end)
