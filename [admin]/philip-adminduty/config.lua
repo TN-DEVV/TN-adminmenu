1655be2c0bd41e8642830f4fce80e36cff0c2f70
DON <93664733+TN-DEVV@users.noreply.github.com>
2023-09-18 10:59:41 +0100

    Update config.lua

--- [admin]/philip-adminduty/config.lua
+++ [admin]/philip-adminduty/config.lua
@@ -6,7 +6,7 @@ Config = {
     AdminLogoHeight = 1.5, -- How height must be the logo above the head!
     LogoSpin = false, -- Change this to false if you don't want the logo to spin and change true if you want the logo to spin.
     LogoMovingWithCamera = true, -- Change this to false if you don't want the logo to look always the player camera direction and change true if you want the logo to look always the player camera direction.
-    LogoBouncing = false -- Change this to false if you don't want the logo to be bouncing and change true if you want the logo to be bouncing.
+    LogoBouncing = false, -- Change this to false if you don't want the logo to be bouncing and change true if you want the logo to be bouncing.
     clothes = "qb-clothing" -- choose your clothing script qb-clothing / qb-clothes / illenium-appearance
     -- NOTE: You can't use LogoSpin and LogoMovingWithCamera at the same time. It's not gonna work!
 
