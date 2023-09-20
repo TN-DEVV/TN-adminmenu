3c79dfbbcca1e07e84de56d1cff834cef5a41dda
DON <93664733+TN-DEVV@users.noreply.github.com>
2023-09-20 12:31:57 +0100

    Update permissions.sql

--- permissions.sql
+++ permissions.sql
@@ -1,11 +1,2 @@
-CREATE TABLE IF NOT EXISTS `adminmenu` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `license` varchar(255) DEFAULT NULL,
-  `citizenid` varchar(255) DEFAULT NULL,
-  `permission` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
-
 ALTER TABLE players
 ADD COLUMN adminduty INT DEFAULT 0;
